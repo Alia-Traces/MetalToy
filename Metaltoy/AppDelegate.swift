ee253f5af0866f6c42727e910eac2c28bc62394f
Psonice <psonice@me.com>
2017-02-27 21:55:32 +0000

    Initial Commit

---
+++ Metaltoy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Metaltoy
+//
+//  Created by Chris Wood on 27/02/2017.
+//  Copyright © 2017 Interealtime. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
